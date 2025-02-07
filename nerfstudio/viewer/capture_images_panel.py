67c51d20613716d42ac094a5d1dda0abb89f321a
Adriano Cicco <adrianocicco11@gmail.com>
2025-02-07 09:27:33 +0100

    refactor panel

--- nerfstudio/viewer/capture_images_panel.py
+++ nerfstudio/viewer/capture_images_panel.py
@@ -1,4 +1,5 @@
 import math
+import pickle
 from typing import List
 import torch
 from viser import ViserServer
@@ -9,60 +10,91 @@ from nerfstudio.models.base_model import Model
 from nerfstudio.viewer.render_panel import CameraPath, Keyframe
 from nerfstudio.viewer.render_state_machine import RenderStateMachine
 from nerfstudio.viewer.utils import CameraState
-from nerfstudio.viewer.viewer_elements import ViewerButton, ViewerCheckbox, ViewerDropdown, ViewerNumber, ViewerSlider, ViewerText, ViewerVec3
+from nerfstudio.viewer.viewer_elements import (
+    ViewerButton,
+    ViewerCheckbox,
+    ViewerDropdown,
+    ViewerNumber,
+    ViewerSlider,
+    ViewerText,
+    ViewerVec3,
+)
 from scipy.spatial.transform import Rotation
 from websockets.sync.client import connect
 
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+class SocketMessage:
+
+    def __init__(self, type: str, message: str):
+        self.type = type
+        self.message = message
+
+    def to_pickle(self):
+        return pickle.dumps(self)
+
+
 class CaptureImagesPanel:
 
-    def __init__(
-            self,
-            server : ViserServer,
-            viewer
-    ):
+    def get_pipeline_websocket(self):
+        try:
+            if self.pipeline_websocket is None:
+                self.pipeline_websocket = connect("ws://localhost:8765")
+        except:
+            self.pipeline_websocket = None
+        return self.pipeline_websocket
+
+    def __init__(self, server: ViserServer, viewer):
         self.server = server
         self.viewer = viewer
         self.step = 0
-        self.file_socket = connect("ws://localhost:8765")
-        self.websocket = connect("ws://localhost:8766")  # Crea connessione persistente
+        self.prospective_file_dict = {
+            "Top": "top_camera.png",
+            "Right Top": "right_top.png",
+            "Back Right Top": "back_right_top.png",
+            "Back Top": "back_top.png",
+            "Back Left Top": "back_left_top.png",
+            "Left Top": "left_top.png",
+            "Front Left Top": "front_left_top.png",
+            "Front Top": "front_top.png",
+            "Front Right Top": "front_right_top.png",
+            "Right": "right.png",
+            "Back Right": "back_right.png",
+            "Back": "back.png",
+            "Back Left": "back_left.png",
+            "Left": "left.png",
+            "Front Left": "front_left.png",
+            "Front": "front.png",
+            "Front Right": "front_right.png",
+        }
 
-        self.a = ViewerButton(name="My Button", cb_hook= lambda han: self.handle_btn())
-        self.h = ViewerButton(name="Toggle training", cb_hook= lambda han: self.toggle_training_btn())
-
-        self.b = ViewerNumber(name="Number", default_value=1.0)
-        self.c = ViewerCheckbox(name="Checkbox", default_value=False)
-        self.d = ViewerDropdown(name="Dropdown", default_value="A", options=["A", "B"])
-        self.e = ViewerSlider(name="Slider", default_value=0.5, min_value=0.0, max_value=1.0)
-        self.f = ViewerText(name="Text", default_value="Hello World")
-        self.g = ViewerVec3(name="3D Vector", default_value=(0.1, 0.7, 0.1), visible=True)
+        self.a = ViewerButton(name="Render", cb_hook=lambda _: self.output_rendering())
+        self.b = ViewerButton(
+            name="Toggle training", cb_hook=lambda _: self.toggle_training_btn()
+        )
 
         with self.server.gui.add_folder("TestOptions"):
             self.a.install(self.server)
-            self.h.install(self.server)
             self.b.install(self.server)
-            self.c.install(self.server)
-            self.d.install(self.server)
-            self.e.install(self.server)
-            self.f.install(self.server)
-            self.g.install(self.server)
 
         self.base_dirs = {
-            "Front":  torch.tensor([0.0, -1.0,  0.0], dtype=torch.float32),
-            "Back":   torch.tensor([0.0, 1.0,  0.0], dtype=torch.float32),
-            "Right":  torch.tensor([1.0,  0.0,  0.0], dtype=torch.float32),
-            "Left":   torch.tensor([-1.0, 0.0,  0.0], dtype=torch.float32),
-            "Top":    torch.tensor([0.0,  0.0,  1.0], dtype=torch.float32),
+            "Front": torch.tensor([0.0, -1.0, 0.0], dtype=torch.float32),
+            "Back": torch.tensor([0.0, 1.0, 0.0], dtype=torch.float32),
+            "Right": torch.tensor([1.0, 0.0, 0.0], dtype=torch.float32),
+            "Left": torch.tensor([-1.0, 0.0, 0.0], dtype=torch.float32),
+            "Top": torch.tensor([0.0, 0.0, 1.0], dtype=torch.float32),
         }
         self.cameras = self.generate_multiple_camera_states()
+
     def toggle_training_btn(self) -> None:
         self.viewer.toggle_pause_button()
         self.viewer._toggle_training_state(None)
 
-
-    def set_render_state_machine(self, render_state_machine: RenderStateMachine) -> None:
+    def set_render_state_machine(
+        self, render_state_machine: RenderStateMachine
+    ) -> None:
         self.render_state_machine = render_state_machine
 
     def set_client(self, client: viser.ClientHandle) -> None:
@@ -71,50 +103,49 @@ class CaptureImagesPanel:
     def set_step(self, step: int) -> None:
         self.step = step
 
-        if(self.step % 20 == 0):
+        if self.step % 50 == 0:
             print(f"Step: {step}")
-            with connect("ws://localhost:8766") as websocket:
-                websocket.send(f"step {step}")
+            try:
+                with connect("ws://localhost:8765") as websocket:
+                    websocket.send(SocketMessage("step", str(step)).to_pickle())
+            except Exception as e:
+                print(e)
+                pass
 
-
-        if(self.step > 3400):
+        if self.step > 2000:
             self.viewer.toggle_pause_button()
             self.viewer._toggle_training_state(None)
+            self.output_rendering()
 
-            self.handle_btn()
+    def send_status_to_pipeline(self, camera_file_name: str) -> None:
+        try:
+            with connect("ws://localhost:8765") as websocket:
+                websocket.send(SocketMessage("camera", camera_file_name).to_pickle())
+        except:
+            pass
 
-
-    def handle_btn(self) -> None:
-        print("Button clicked!")
+    def output_rendering(self) -> None:
         for camera in self.cameras:
             print(f"Rendering image for camera: {camera.name}")
 
             self.render_state_machine.state = "high"
-            image=self.render_state_machine._render_img(camera)["rgb"].cpu().numpy()
+            image = self.render_state_machine._render_img(camera)["rgb"].cpu().numpy()
             # save image as png
             image_array = image  # È già in formato numpy grazie a .cpu().numpy()
 
-            dictionary={'Top': 'top_camera.png', 'Right Top': 'right_top.png', 'Back Right Top': 'back_right_top.png', 'Back Top': 'back_top.png', 'Back Left Top': 'back_left_top.png', 'Left Top': 'left_top.png', 'Front Left Top': 'front_left_top.png', 'Front Top': 'front_top.png', 'Front Right Top': 'front_right_top.png', 'Right': 'right.png', 'Back Right': 'back_right.png', 'Back': 'back.png', 'Back Left': 'back_left.png', 'Left': 'left.png', 'Front Left': 'front_left.png', 'Front': 'front.png', 'Front Right': 'front_right.png'}
-
-            file_name = f"output_{dictionary[camera.name]}"
+            file_name = f"output_{self.prospective_file_dict[camera.name]}"
             # Converti l'array numpy in un'immagine PIL
-            image_pil = Image.fromarray((image_array * 255).astype(np.uint8))  # Sa a [0, 255] se l'array è in [0, 1]
+            image_pil = Image.fromarray(
+                (image_array * 255).astype(np.uint8)
+            )  # Sa a [0, 255] se l'array è in [0, 1]
             image_pil.save(file_name)
 
-            def hello():
-                with connect("ws://localhost:8765") as websocket:
-                    websocket.send(dictionary[camera.name])
-                    message = websocket.recv()
-                    print(f"Received: {message}")
-
-            hello() 
+            self.send_status_to_pipeline(file_name)
 
             print("camera_matrix:", camera.c2w)
             print("position:", camera.fov)
             print("aspect:", camera.aspect)
-            
 
-    
     def get_c2w_matrix(self, direction: str) -> torch.Tensor:
         """
         Ritorna la matrice 4x4 camera-to-world (C2W) per la telecamera
@@ -135,13 +166,17 @@ class CaptureImagesPanel:
         pos = torch.zeros(3, dtype=torch.float32)
         for w in words:
             if w not in self.base_dirs:
-                raise ValueError(f"La parola '{w}' non è tra le direzioni base supportate.")
+                raise ValueError(
+                    f"La parola '{w}' non è tra le direzioni base supportate."
+                )
             pos += self.base_dirs[w]
 
         # Normalizza per avere raggio 1
         norm = torch.norm(pos)
         if norm < 1e-8:
-            raise ValueError(f"Il vettore posizione risulta nullo per la direzione '{direction}'.")
+            raise ValueError(
+                f"Il vettore posizione risulta nullo per la direzione '{direction}'."
+            )
         pos = pos / norm  # raggio unitaria
 
         # 2. Definisci il target (centro sfera) e l'up world
@@ -157,7 +192,7 @@ class CaptureImagesPanel:
         #    in modo che l'asse Z in camera space punti "in avanti" dal punto di vista della camera.
         #    Se invece preferiamo che la camera guardi lungo -Z in camera space,
         #    possiamo invertire "forward" (eye - target) => - (target - eye).
-        
+
         # In questo esempio, definiamo la Z locale della camera come (eye - target) normalizzata.
         eye = pos  # la camera è posizionata su 'pos'
         forward = eye - target
@@ -195,103 +230,47 @@ class CaptureImagesPanel:
         c2w_3x4 = torch.cat([R, T], dim=1)  # shape (3,4)
         return c2w_3x4
 
-
-
     # Utility function to create camera state
-    def create_camera_state(self, fov: float, aspect: float, direction: str, camera_type: CameraType) -> CameraState:
+    def create_camera_state(
+        self, fov: float, aspect: float, direction: str, camera_type: CameraType
+    ) -> CameraState:
         """Creates a CameraState for a given position."""
         # Default translation for the camera to be placed 1 unit away from the origin
-                # Combine rotation and translation to form c2w matrix
-        
+        # Combine rotation and translation to form c2w matrix
 
         # get camera matrix from string
         camera_matrix = self.get_c2w_matrix(direction)
         c2w = torch.tensor(camera_matrix, dtype=torch.float64)
 
         # Create the CameraState instance
-        return CameraState(fov=fov, aspect=aspect, c2w=c2w, camera_type=
+        return CameraState(
+            fov=fov,
+            aspect=aspect,
+            c2w=c2w,
+            camera_type=(
                 CameraType.PERSPECTIVE
                 if camera_type == "Perspective"
-                else CameraType.FISHEYE
-                if camera_type == "Fisheye"
-                else CameraType.EQUIRECTANGULAR
-                if camera_type == "Equirectangular"
-                else CameraType.PERSPECTIVE,
-                name=direction
+                else (
+                    CameraType.FISHEYE
+                    if camera_type == "Fisheye"
+                    else (
+                        CameraType.EQUIRECTANGULAR
+                        if camera_type == "Equirectangular"
+                        else CameraType.PERSPECTIVE
+                    )
                 )
-
-
-    def decompose_c2w(self,matrix):
-        """
-        Decomposes a 4x4 camera-to-world (C2W) transformation matrix into position and quaternion.
-        
-        Parameters:
-            matrix (np.ndarray): 4x4 transformation matrix.
-            
-        Returns:
-            position (np.ndarray): 3D position vector.
-            quaternion (np.ndarray): Quaternion (x, y, z, w).
-        """
-        # Extract the translation (position)
-        position = matrix[:3, 3]
-        position *= 10.0
-
-        # Subtract the offset (0, 0, 0.3)
-        #offset = torch.tensor([0.0, 0.0, 0.5*10], dtype=position.dtype, device=position.device)
-        #position -= offset
-
-        # Extract the rotation matrix (upper-left 3x3 part)
-        rotation_matrix = matrix[:3, :3]
-
-        # Convert rotation matrix to quaternion (SciPy uses (x, y, z, w) format)
-        quaternion = Rotation.from_matrix(rotation_matrix).as_quat()
-
-        return position, quaternion
-
+            ),
+            name=direction,
+        )
 
     def generate_multiple_camera_states(self) -> List[CameraState]:
-        # Example usage
-        fov =  0.6911111611634243  # Field of view in radians
+        fov = 0.6911111611634243  # Field of view in radians
         aspect = 1  # Aspect ratio
         camera_type = CameraType.PERSPECTIVE
+        directions = self.prospective_file_dict.keys()
 
-        directions = [
-        "Top",
-        "Right Top",
-        "Back Right Top",
-        "Back Top",
-        "Back Left Top",
-        "Left Top",
-        "Front Left Top",
-        "Front Top",
-        "Front Right Top",
-        "Right",
-        "Back Right",
-        "Back",
-        "Back Left",
-        "Left",
-        "Front Left",
-        "Front",
-        "Front Right" ]
-
-        camera_states = [self.create_camera_state(fov, aspect, direction, camera_type) for direction in directions]
-        #
-        #for camera_state in camera_states:
-        #    positon, quaternion = self.decompose_c2w(camera_state.c2w)
-        #    keyframe = Keyframe(
-        #        position=positon,
-        #        wxyz=quaternion,
-        #        override_fov_enabled=False,
-        #        override_fov_rad=camera_state.fov * np.pi / 180.0,
-        #        override_time_enabled=False,
-        #        override_time_val=0.0,
-        #        aspect=camera_state.aspect,
-        #        override_transition_enabled=False,
-        #        override_transition_sec=None
-        #    )
-        #    self.camera_path.add_camera(keyframe)
-                                    
+        camera_states = [
+            self.create_camera_state(fov, aspect, direction, camera_type)
+            for direction in directions
+        ]
         return camera_states
-
-
-    
